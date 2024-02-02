1e0e267e1d4bc57018d90a49e6d0862553f02b12
Faucher Simon <sfaucher@enssat.fr>
2024-02-02 12:04:10 +0100

    Ajout branchement Memory

--- routes/MetricInsight.js
+++ routes/MetricInsight.js
@@ -4,7 +4,6 @@ var path = require('path');
 
 /* get startMetricInsight page. */
 router.get('/', function(req, res, next) {
-    console.log("my_config");
     res.sendFile(path.join(__dirname, '../views/MetricInsight.html'));
 });
 
