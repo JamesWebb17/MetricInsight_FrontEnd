3cfae0747a10c853cf6bc6933535ea36d17a1e78
Faucher Simon <sfaucher@enssat.fr>
2024-02-12 16:41:24 +0100

    Test envoie requete front-end

--- routes/MetricInsight.js
+++ routes/MetricInsight.js
@@ -1,10 +1,27 @@
-var express = require('express');
-var router = express.Router();
-var path = require('path');
+let express = require('express');
+let router = express.Router();
+let path = require('path');
+const {json} = require("express");
+
+let adresseIP = "http://10.0.0.10:3001/";
+
 
 /* get startMetricInsight page. */
 router.get('/', function(req, res, next) {
-    res.sendFile(path.join(__dirname, '../views/MetricInsight.html'));
+    console.log('startMetricInsight');
+    fetch(adresseIP + 'api/hello', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify('ok'),
+    }).then(response => response.json())
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => {
+            console.error('Erreur lors de la requête fetch :', error);
+        })
 });
 
 module.exports = router;
